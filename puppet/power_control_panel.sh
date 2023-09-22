302f9aba36a732b8657db48dc8b5a7877e20306e
Arthur Gareginyan <arthurgareginyan@gmail.com>
2023-09-22 17:47:12 +0300

    Change: power_control_panel.sh

--- puppet/power_control_panel.sh
+++ puppet/power_control_panel.sh
@@ -6,13 +6,13 @@ action="$1"
 if [ "$action" == "poweroff" ]; then
     # Power off the server
     sudo poweroff
-    echo "Server is powering off."
+    echo "Powering off"
 elif [ "$action" == "restart" ]; then
     systemctl reboot
-    echo "Server is restarting."
+    echo "Restarting"
 elif [ "$action" == "status" ]; then
     # Check server status (Always running if this script can be executed)
-    echo "Server is running."
+    echo "Running"
 else
-    echo "Invalid argument. Use 'poweroff' or 'status'."
+    echo "Invalid argument. Use 'poweroff' or 'restart' or 'status'."
 fi
